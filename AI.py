ef3e9fa633600d30ad6e89a78138e8df1cf97dcc
Yuato <tonyyuan1010@gmail.com>
2024-03-23 01:01:55 -0400

    Changes to AI.py, ploted mp3 file into a wave graph using matplotlib

--- AI.py
+++ AI.py
@@ -1,31 +1,46 @@
 import os
-from matplotlib import pyplot as pyplot
+from matplotlib import pyplot as plt
 import tensorflow as tf
 import tensorflow_io as tfio
 
-CAPUCHIN_FILE = os.path.join('data', 'ParsedCapuchinebird_Clips', 'XC3776-3.wav')
-NOT_CAPUCHIN_FILE = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips', 'afternoon-birds-song-in-forst-0.wav')
+CAPUCHIN_FILE = os.path.join('data', 'Parsed_Capuchinbird_Clips', 'XC3776-3.wav')
+NOT_CAPUCHIN_FILE = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips', 'afternoon-birds-song-in-forest-0.wav')
 
 print(CAPUCHIN_FILE)
+print(NOT_CAPUCHIN_FILE)
+
+file_contents = tf.io.read_file(CAPUCHIN_FILE)
 
 def load_wav_16k_mono(filename):
+    # Load encoded wav file
     file_contents = tf.io.read_file(filename)
-    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels = 1)
+    # Decode wav (tensors by channels) 
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    # Removes trailing axis
     wav = tf.squeeze(wav, axis=-1)
     sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Goes from 44100Hz to 16000hz - amplitude of the audio signal
     wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
     return wav
+
+wave = load_wav_16k_mono(CAPUCHIN_FILE)
+nwave = load_wav_16k_mono(NOT_CAPUCHIN_FILE)
+
+plt.plot(wave)
+plt.plot(nwave)
+plt.show()
 
 POS = os.path.join('data', 'Parsed_Capuchinbird_Clips')
 NEG = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips')
 
 pos = tf.data.Dataset.list_files(POS+'\*.wav')
+pos.as_numpy_iterator().next()
 neg = tf.data.Dataset.list_files(NEG+'\*.wav')
 
 pos.as_numpy_iterator().next()
 
-positives = tf.data.Dataset.sip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
-negatives = tf.data.Dataset.sip((neg, tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
+positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
+negatives = tf.data.Dataset.zip((neg, tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
 positives.concatenate(negatives)
 
-len(pos)
+print(tf.ones(len(pos)))
